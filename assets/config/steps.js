ebf9f5d15ae8f9b6c172f5efbee06cbaccad3533
Peter Fisher <hello@websomatic.co.uk>
2016-03-03 20:20:41 +0000

    Adding more steps

--- assets/config/steps.js
+++ assets/config/steps.js
@@ -7,18 +7,19 @@ var stepsConfig = [
     {
         "name": "Old Kent Road",
         "type": "street",
-        "value": 140,
+        "value": 60,
     },
     {
         "name": "Community Chest",
         "type": "community_chest",
-        "description": "Community Chest Description 1",
-        "value": 0,
+        "description": "Receive £25 consultancy fee",
+        "value": 25,
+        "fee_type": "income"
     },
     {
         "name": "White Chapel Road",
         "type": "street",
-        "value": 200,
+        "value": 60,
     },
     {
         "name": "Income Tax",
@@ -38,8 +39,9 @@ var stepsConfig = [
     {
         "name": "Chance",
         "type": "chance",
-        "description": "Chance Description 1",
-        "value": 0,
+        "description": "Speeding Fine £15",
+        "value": 15,
+        "fee_type": "expense"
     },
     {
         "name": "Euston Road",
@@ -50,12 +52,11 @@ var stepsConfig = [
         "name": "Pentonvile Road",
         "type": "street",
         "value": 120,
-        "route": "to_owner"
     },
     {
         "name": "Jail",
         "type": "jail",
-        "value": 200,
+        "value": 0,
     },
     {
         "name": "Pall Mall",
@@ -76,5 +77,138 @@ var stepsConfig = [
         "name": "Northumberland Avenue",
         "type": "street",
         "value": 160,
-    }
+    },
+    {
+        "name": "Marylebone Station",
+        "type": "street",
+        "value": 200,
+    },
+    {
+        "name": "Bow Street",
+        "type": "street",
+        "value": 180,
+    },
+    {
+        "name": "Community Chest",
+        "description": "Income tax Refund Collect £20",
+        "type": "community_chest",
+        "value": 20,
+        "fee_type": "income"
+    },
+    {
+        "name": "Marlborough Street",
+        "type": "street",
+        "value": 180,
+    },
+    {
+        "name": "Vine Street",
+        "type": "street",
+        "value": 200,
+    },
+    {
+        "name": "Free Parking",
+        "type": "free_parking",
+        "value": 0,
+    },
+    {
+        "name": "Strand",
+        "type": "free_parking",
+        "value": 220,
+    },
+    {
+        "name": "Chance",
+        "type": "chance",
+        "description": "Bank pay you dividend of £50",
+        "value": 50,
+        "fee_type": "income"
+    },
+    {
+        "name": "Fleet Street",
+        "type": "street",
+        "value": 220,
+    },
+    {
+        "name": "Trafalgar Sqaure",
+        "type": "street",
+        "value": 240,
+    },
+    {
+        "name": "Fenchurch Street Station",
+        "type": "street",
+        "value": 200,
+    },
+    {
+        "name": "Leicester Square",
+        "type": "street",
+        "value": 260,
+    },
+    {
+        "name": "Coventry Street",
+        "type": "street",
+        "value": 260,
+    },
+    {
+        "name": "Water Works",
+        "type": "street",
+        "value": 150,
+    },
+    {
+        "name": "Piccadilly",
+        "type": "street",
+        "value": 280,
+    },
+    {
+        "name": "Go To Jail",
+        "type": "go_to_jail",
+        "value": 0,
+    },
+    {
+        "name": "Regent Street",
+        "type": "street",
+        "value": 300,
+    },
+    {
+        "name": "Oxford Street",
+        "type": "street",
+        "value": 300,
+    },
+    {
+        "name": "Community Chest ",
+        "type": "community_chest",
+        "description": "School Fees",
+        "value": 50,
+        "fee_type": "expense"
+    },
+    {
+        "name": "Bond Street",
+        "type": "street",
+        "value": 320,
+    },
+    {
+        "name": "Liverpool Street Station",
+        "type": "street",
+        "value": 200,
+    },
+    {
+        "name": "Chance",
+        "type": "chance",
+        "value": 15,
+        "fee_type": "expense",
+        "description": "Speeding Fine"
+    },
+    {
+        "name": "Park Lane",
+        "type": "street",
+        "value": 350,
+    },
+    {
+        "name": "Super Tax",
+        "type": "street",
+        "value": 100,
+    },
+    {
+        "name": "Mayfair",
+        "type": "street",
+        "value": 400,
+    },
 ];
