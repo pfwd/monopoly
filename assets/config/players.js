4d6ec178d12c922e38d2dd11ba05e203c7ba3b12
Peter Fisher <hello@websomatic.co.uk>
2016-03-03 16:17:44 +0000

    initial commit

---
+++ assets/config/players.js
@@ -0,0 +1,16 @@
+var playersConfig = [
+    {
+        "name": "Computer",
+        "id": 1,
+        "funds": 1500,
+        "isComputer": true,
+        "cssClass":"computer"
+    },
+     {
+        "name": "Peter Fisher",
+        "id": 2,
+        "funds": 1500,
+        "isComputer":false,
+        "cssClass":"player"
+    }
+];
